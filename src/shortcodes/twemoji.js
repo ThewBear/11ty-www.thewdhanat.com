b2a27909dea245f3a99bfc2d53033c7520a038fa
ThewBear <26767607+ThewBear@users.noreply.github.com>
2020-08-02 20:42:34 +0700

    Update twemoji shortcode - Accept multiple emojis

--- src/shortcodes/twemoji.js
+++ src/shortcodes/twemoji.js
@@ -1,6 +1,11 @@
 const twemoji = require("twemoji");
+const regex = require('twemoji-parser/dist/lib/regex').default;
+const UFE0Fg = /\uFE0F/g
 
-module.exports = function (emoji, className = "") {
-  const codePoint = twemoji.convert.toCodePoint(emoji);
-  return `<img class="twemoji ${className}" draggable="false" src="${twemoji.base}svg/${codePoint}.svg" alt="${emoji}">`;
+module.exports = function (emoji, {className = "", alt = true} = {}) {
+  // https://github.com/twitter/twemoji/blob/master/scripts/build.js
+  return emoji.match(regex).reduce((prev, curr) => {
+    const codePoint = twemoji.convert.toCodePoint(curr.replace(UFE0Fg, ""));
+    return prev + `<img class="twemoji ${className}" draggable="false" src="${twemoji.base}svg/${codePoint}.svg" alt="${alt ? curr : ""}">`;
+  }, "")
 };
