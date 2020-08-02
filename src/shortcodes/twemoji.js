a7a3bd2a4b9be82be27a508be29e128d85ca5779
ThewBear <26767607+ThewBear@users.noreply.github.com>
2020-08-02 21:25:31 +0700

    Fix lint

--- src/shortcodes/twemoji.js
+++ src/shortcodes/twemoji.js
@@ -1,11 +1,16 @@
 const twemoji = require("twemoji");
-const regex = require('twemoji-parser/dist/lib/regex').default;
-const UFE0Fg = /\uFE0F/g
+const regex = require("twemoji-parser/dist/lib/regex").default;
+const UFE0Fg = /\uFE0F/g;
 
-module.exports = function (emoji, {className = "", alt = true} = {}) {
+module.exports = function (emoji, { className = "", alt = true } = {}) {
   // https://github.com/twitter/twemoji/blob/master/scripts/build.js
   return emoji.match(regex).reduce((prev, curr) => {
     const codePoint = twemoji.convert.toCodePoint(curr.replace(UFE0Fg, ""));
-    return prev + `<img class="twemoji ${className}" draggable="false" src="${twemoji.base}svg/${codePoint}.svg" alt="${alt ? curr : ""}">`;
-  }, "")
+    return (
+      prev +
+      `<img class="twemoji ${className}" draggable="false" src="${
+        twemoji.base
+      }svg/${codePoint}.svg" alt="${alt ? curr : ""}">`
+    );
+  }, "");
 };
