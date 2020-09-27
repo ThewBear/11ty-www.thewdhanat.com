051c2ce0ca84db6b8effece4e4362ed5785ba34d
ThewBear <26767607+ThewBear@users.noreply.github.com>
2020-09-27 16:19:27 +0700

    Fix twomoji zero-width joiner U200D

--- src/filters/twemoji.js
+++ src/filters/twemoji.js
@@ -5,7 +5,9 @@ const UFE0Fg = /\uFE0F/g;
 module.exports = function (content) {
   // https://github.com/twitter/twemoji/blob/master/scripts/build.js
   return content.replace(regex, (match) => {
-    const codePoint = twemoji.convert.toCodePoint(match.replace(UFE0Fg, ""));
+    const codePoint = twemoji.convert.toCodePoint(
+      match.indexOf("\u200D") >= 0 ? match : match.replace(UFE0Fg, "")
+    );
     return `<img class="twemoji" draggable="false" src="${twemoji.base}svg/${codePoint}.svg" alt="${match}">`;
   });
 };
--- src/shortcodes/twemoji.js
+++ src/shortcodes/twemoji.js
@@ -5,7 +5,9 @@ const UFE0Fg = /\uFE0F/g;
 module.exports = function (emoji, { className = "", alt = true } = {}) {
   // https://github.com/twitter/twemoji/blob/master/scripts/build.js
   return emoji.match(regex).reduce((prev, curr) => {
-    const codePoint = twemoji.convert.toCodePoint(curr.replace(UFE0Fg, ""));
+    const codePoint = twemoji.convert.toCodePoint(
+      curr.indexOf("\u200D") >= 0 ? curr : curr.replace(UFE0Fg, "")
+    );
     return (
       prev +
       `<img class="twemoji ${className}" draggable="false" src="${
