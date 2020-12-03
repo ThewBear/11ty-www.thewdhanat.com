02b954205151bd2a2bf7e6caf57d64b00a79b7fa
ThewBear <26767607+ThewBear@users.noreply.github.com>
2020-12-03 23:09:40 +0700

    Add course certificates

---
+++ src/index.11tydata.js
@@ -0,0 +1,35 @@
+module.exports = {
+  online_certificates: [
+    {
+      url: "",
+      title: "AI for Medicine",
+      platform: "Coursera",
+    },
+    {
+      url:
+        "https://www.coursera.org/account/accomplishments/certificate/QJRDNMFVA2AF",
+      title: "AI For Everyone",
+      platform: "Coursera",
+    },
+    {
+      url:
+        "https://www.coursera.org/account/accomplishments/specialization/certificate/59DUGN2Y2USV",
+      title: "Cloud Engineering with Google Cloud",
+      platform: "Coursera",
+      notes: "5 courses",
+    },
+    {
+      url:
+        "https://www.coursera.org/account/accomplishments/specialization/certificate/MH7WBBF38WPH",
+      title: "Data Engineering with Google Cloud",
+      platform: "Coursera",
+      notes: "6 courses",
+    },
+    {
+      url:
+        "https://www.datacamp.com/statement-of-accomplishment/track/0a8ccad7613bc34fdff3cca2143dcd228dc8e58d",
+      title: "Data Scientist with Python Track",
+      platform: "datacamp",
+    },
+  ],
+};
