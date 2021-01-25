1af24175885e57e2fac75796ae92102253081cd6
ThewBear <26767607+ThewBear@users.noreply.github.com>
2021-01-26 00:04:01 +0700

    Fix lint

--- src/index.11tydata.js
+++ src/index.11tydata.js
@@ -1,7 +1,8 @@
 module.exports = {
   online_certificates: [
     {
-      url: "https://www.coursera.org/account/accomplishments/specialization/certificate/9PWKSNT8CJKA",
+      url:
+        "https://www.coursera.org/account/accomplishments/specialization/certificate/9PWKSNT8CJKA",
       title: "AI for Medicine",
       platform: "Coursera",
     },
