9ac9728d45b65817f0ee1d3d3686d575dfd8f2d9
ThewBear <26767607+ThewBear@users.noreply.github.com>
2021-04-16 00:03:52 +0700

    Add more awesome

--- src/awesome/awesome.11tydata.js
+++ src/awesome/awesome.11tydata.js
@@ -4,20 +4,45 @@ const programmings = [
     description: "General programming",
     items: [
       {
-        title: "Freecodecamp",
-        description: "Learn to code. Build projects. Earn certifications.",
-        url: "https://www.freecodecamp.org/",
-      },
-      {
         title: "Coding Interview University",
         description:
           "A complete computer science study plan to become a software engineer.",
         url: "https://github.com/jwasham/coding-interview-university#readme",
+      },
+      {
+        title: "Freecodecamp",
+        description: "Learn to code. Build projects. Earn certifications.",
+        url: "https://www.freecodecamp.org/",
+      },
+    ],
+  },
+  {
+    title: "Books",
+    description: "Computer science",
+    items: [
+      {
+        title: "Electronic References",
+        description:
+          "CS textbooks in a variety of areas that are freely available online.",
+        url: "https://csgordon.github.io/books.html",
+      },
+    ],
+  },
+
+  {
+    title: "Tools",
+    description: "",
+    items: [
+      {
+        title: "Free for developers",
+        description:
+          "This is a list of software (SaaS, PaaS, IaaS, etc.) and other offerings that have free tiers for developers.",
+        url: "https://free-for.dev/",
       },
     ],
   },
 ];
 
 module.exports = {
-  programmings,
+  contents: new Map([["General Programming", programmings]]),
 };
