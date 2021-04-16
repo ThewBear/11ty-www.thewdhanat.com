82ee7f7654545f8517d3d327ea559b55462f4d89
ThewBear <26767607+ThewBear@users.noreply.github.com>
2021-04-16 11:37:11 +0700

    Increase deviceScaleFactor to 2

--- src/shortcodes/urlThumbnail.js
+++ src/shortcodes/urlThumbnail.js
@@ -30,7 +30,7 @@ module.exports = async function urlThumbnail(
   const thumbnailFile = `_site/_urlThumbnail/${slug}.png`;
   const browser = await chromium.launch();
   const page = await browser.newPage({
-    deviceScaleFactor: IS_PRODUCTION ? 1.5 : 1,
+    deviceScaleFactor: IS_PRODUCTION ? 2 : 1,
     viewport: {
       width: 1440,
       height: 1440 * 2.5,
