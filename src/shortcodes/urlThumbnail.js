36888a01999f1ded0ccd30c9fd6b0988ebc71431
ThewBear <26767607+ThewBear@users.noreply.github.com>
2021-04-16 11:05:01 +0700

    Set UV_THREADPOOL_SIZE = 1

--- src/shortcodes/urlThumbnail.js
+++ src/shortcodes/urlThumbnail.js
@@ -4,7 +4,7 @@ const Image = require("@11ty/eleventy-img");
 
 const IS_PRODUCTION = process.env.NODE_ENV === "production";
 
-process.env.UV_THREADPOOL_SIZE = 2; // https://github.com/lovell/sharp/issues/138
+process.env.UV_THREADPOOL_SIZE = 1; // https://github.com/lovell/sharp/issues/138
 
 const cached = {};
 
