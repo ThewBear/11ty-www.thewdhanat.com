8bc44e788d7e61b00a3e2fb9705afb0f5a550b28
ThewBear <26767607+ThewBear@users.noreply.github.com>
2021-07-24 02:01:50 +0700

    Update awesome.mjs

--- bin/awesome.mjs
+++ bin/awesome.mjs
@@ -1,38 +1,40 @@
-import crypto from "crypto"
-import data from "../src/awesome/awesome.11tydata.js"
-import PQueue from "p-queue"
-import slugify from "slugify"
-import { chromium } from "playwright"
+import crypto from "crypto";
+import fs from "fs/promises";
+import data from "../src/awesome/awesome.11tydata.js";
+import PQueue from "p-queue";
+import slugify from "slugify";
+import { chromium } from "playwright";
 
-const queue = new PQueue.default({ concurrency: 5 })
-const chrome = await chromium.launch()
-queue.on("idle", () => {
-  chrome.close()
-})
+const queue = new PQueue.default({ concurrency: 5 });
+const chrome = await chromium.launch();
+queue.on("idle", async () => {
+  await chrome.close();
+  await fs.writeFile("_data/awesome/data.json", JSON.stringify(pages));
+});
 
-const pages = {}
+const pages = {};
 
 data.contents.forEach((value) => {
   value.forEach((topic) =>
     topic.items.forEach(({ url }) => (pages[url] = null))
-  )
-})
+  );
+});
 
 Object.keys(pages).forEach((url) => {
   if (pages[url]) {
-    return pages[url]
+    return pages[url];
   }
 
-  const slug = slugify(url.replace(/https?:\/\/|[.]/g, ""))
+  const slug = slugify(url.replace(/https?:\/\/|[.]/g, ""));
 
   const metadata = {
     width: 1440,
     height: 1440 * 2,
     format: "png",
-  }
+  };
 
-  const randomString = crypto.randomBytes(4).toString("hex")
-  const thumbnailFile = `${slug}_${randomString}.png`
+  const randomString = crypto.randomBytes(4).toString("hex");
+  const thumbnailFile = `${slug}_${randomString}.png`;
 
   queue.add(async () => {
     const page = await chrome.newPage({
@@ -40,19 +42,19 @@ Object.keys(pages).forEach((url) => {
         width: metadata.width,
         height: metadata.height,
       },
-    })
-    await page.goto(url)
-    await page.waitForTimeout(500)
+    });
+    await page.goto(url);
+    await page.waitForTimeout(500);
     await page.screenshot({
       path: `_data/awesome/${thumbnailFile}`,
       type: metadata.format,
-    })
-    await page.close()
-  })
+    });
+    await page.close();
+  });
   pages[url] = {
     slug,
     thumbnailFile,
-  }
-})
+  };
+});
 
-console.log(pages)
+console.log(pages);
