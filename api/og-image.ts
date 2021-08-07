540220324268bf1123cada10bd7fa279ed027c3b
ThewBear <26767607+ThewBear@users.noreply.github.com>
2021-08-08 00:20:13 +0700

    Add open graph

---
+++ api/og-image.ts
@@ -0,0 +1,34 @@
+import { chromium } from "playwright";
+import { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default async function (req: VercelRequest, res: VercelResponse) {
+  const urlPath = req.query.url as string | undefined;
+  if (!urlPath || !urlPath.startsWith("/")) {
+    return res.status(400).end();
+  }
+  const browser = await chromium.launch();
+  const context = await browser.newContext({
+    deviceScaleFactor: 2.5,
+    viewport: { width: 1200, height: 628 },
+  });
+  const page = await context.newPage();
+  const response = await page
+    .goto(
+      `${req.headers["x-forwarded-proto"] || "https"}://${
+        process.env.VERCEL_URL
+      }${urlPath}`
+    )
+    .catch(() => {});
+  if (!response || response.status() !== 200) {
+    return res.status(404).end();
+  }
+  res.setHeader("Content-Type", `image/png`);
+  res.setHeader(
+    "Cache-Control",
+    `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`
+  );
+  res.setHeader("x-size", JSON.stringify(page.viewportSize()));
+  const file = await page.screenshot();
+  await browser.close();
+  return res.end(file);
+}
