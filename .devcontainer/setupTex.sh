5014d226c406cdb6307567eecef5b110cfbd7b05
ThewBear <26767607+ThewBear@users.noreply.github.com>
2024-03-31 15:58:04 +0000

    Fix Sarabun font downloading https://github.com/google/fonts/issues/7481

--- .devcontainer/setupTex.sh
+++ .devcontainer/setupTex.sh
@@ -1,6 +1,19 @@
 #! /bin/bash
 set -euo pipefail
 
-wget -O ~/Sarabun.zip https://fonts.google.com/download?family=Sarabun
-unzip ~/Sarabun.zip -d ~/.fonts/
+# Download Sarabun font
+# Download the JSON file
+curl -s "https://fonts.google.com/download/list?family=Sarabun" > cv/sarabun_metadata_raw.txt
+# Remove ")]}'" from the beginning of the file
+tail -c +5 cv/sarabun_metadata_raw.txt > cv/sarabun_metadata.json
+# Extract the necessary information from the JSON file
+file_refs=$(jq -rc '.manifest.fileRefs[]' cv/sarabun_metadata.json)
+# Download, rename the font files and save to ~/.fonts
+while IFS= read -r file_ref; do
+    filename=$(echo "$file_ref" | jq -r '.filename')
+    url=$(echo "$file_ref" | jq -r '.url')
+    curl -s "$url" -o "~/.fonts/$filename"
+done <<< "$file_refs"
+rm cv/sarabun_*
+
 echo '$xelatex = "xelatex -shell-escape";' >> ~/.latexmkrc
