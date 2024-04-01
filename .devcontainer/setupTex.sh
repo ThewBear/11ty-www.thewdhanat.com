e1c40c1eee9faf9846ce2db64f0e767f2bba0adf
ThewBear <26767607+ThewBear@users.noreply.github.com>
2024-04-01 03:51:40 +0000

    Fix curl in setupTex

--- .devcontainer/setupTex.sh
+++ .devcontainer/setupTex.sh
@@ -2,19 +2,23 @@
 set -euo pipefail
 
 # Download Sarabun font
-# Download the JSON file
 curl -s "https://fonts.google.com/download/list?family=Sarabun" > cv/sarabun_metadata_raw.txt
 # Remove ")]}'" from the beginning of the file
 tail -c +5 cv/sarabun_metadata_raw.txt > cv/sarabun_metadata.json
 # Extract the necessary information from the JSON file
 file_refs=$(jq -rc '.manifest.fileRefs[]' cv/sarabun_metadata.json)
-# Download, rename the font files and save to ~/.fonts
-mkdir -p ~/.fonts
+# Download, save the font file to ~/.fonts
+# We use temp fonts directory since curl cannot write directly to ~/.fonts
+mkdir -p fonts ~/.fonts
 while IFS= read -r file_ref; do
     filename=$(echo "$file_ref" | jq -r '.filename')
     url=$(echo "$file_ref" | jq -r '.url')
-    curl -s "$url" -o "~/.fonts/$filename"
+    echo $url $filename
+    curl "$url" -o "fonts/$filename"
 done <<< "$file_refs"
+mv fonts/* ~/.fonts
+# Clean up
+rmdir fonts
 rm cv/sarabun_*
 
 # echo '$xelatex = "xelatex -shell-escape";' >> ~/.latexmkrc
